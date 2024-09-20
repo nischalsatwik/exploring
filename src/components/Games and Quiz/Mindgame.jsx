054d83ee07254c42f34e4dad7d363fc43e2d4210
satwik <jhsasat@meta.com>
2024-09-20 01:38:12 -0700

    did mind game

--- src/components/Games and Quiz/Mindgame.jsx
+++ src/components/Games and Quiz/Mindgame.jsx
@@ -1,121 +1,121 @@
-import { useState } from "react";
+import {  useRef } from "react";
 function Mindgame()
 {
-        const [vis,setVis]=useState(false);
-        const [message,setMessage]=useState("there is only one winner let's go");
-        const [count,setCount]=useState(1);
-        const [c,setC]=useState(0);
-        const blue=document.getElementsByClassName("blue");
-        const red=document.getElementsByClassName("red");
-        const green=document.getElementsByClassName("green");
-        const yellow=document.getElementsByClassName("yellow");
-        console.log(blue,red,green,yellow);
-        let arr3=[];
-        let arr2=[];
-        function game() 
+    let vis=false;
+    let message="level 1";
+
+    let count=1;
+    let c=0;
+    const blue = useRef(null);
+    const red = useRef(null);
+    const green = useRef(null);
+    const yellow = useRef(null);  
+    let arr3=[];
+    let arr2=[];
+    function game() 
+    {
+        arr3=[];
+        arr2=[];
+        console.log(arr2,"insidegame")
+        glowColors(count, 0);
+    }
+
+    function glowColors(totalCount, currentIndex) 
+    {
+        if (currentIndex < totalCount)
         {
-            arr2 = [];
-            arr3=[];
-            console.log(arr2,"insidegame")
-            glowColors(count, 0);
-        }
- 
-        function glowColors(totalCount, currentIndex) 
-        {
-            if (currentIndex < totalCount)
+            const randomIndex = Math.floor(Math.random() * 4);
+            console.log(randomIndex,":random");
+            if (randomIndex === 0) 
             {
-                const randomIndex = Math.floor(Math.random() * 4);
-                console.log(randomIndex,":random");
-                if (randomIndex === 0) 
-                {
-                    glowAndReset(red, "red");
-                    arr2.push("red");
-                } 
-                else if (randomIndex === 1) 
-                {
-                    glowAndReset(blue, "blue");
-                    arr2.push("blue");
-                } 
-                else if (randomIndex === 2)
-                {
-                    glowAndReset(green, "green");
-                    arr2.push("green");
-                } 
-                else 
-                {
-                    glowAndReset(yellow, "yellow");
-                    arr2.push("yellow");
-                }
-                setTimeout(
-                    () => 
-                    {
-                        glowColors(totalCount, currentIndex + 1);
-                    }, 1500);
-            }    
+                glowAndReset(red, "red");
+                arr2.push("red");
+            } 
+            else if (randomIndex === 1) 
+            {
+                glowAndReset(blue, "blue");
+                arr2.push("blue");
+            } 
+            else if (randomIndex === 2)
+            {
+                glowAndReset(green, "green");
+                arr2.push("green");
+            } 
             else 
             {
-                console.log(arr2,"noted indside glow colors");
-                setVis(true);
+                glowAndReset(yellow, "yellow");
+                arr2.push("yellow");
             }
-        }
- 
-        function glowAndReset(element, color)
-        {
-            console.log(element);
-            element.style.backgroundColor = "black";
-            setTimeout(() => 
-            {
-                element.Style.backgroundColor = color;
-            }, 1000);
-        }
-
-        function coloor(element,color)
-        {   
-            if(vis)
-            {
-               ()=> element.style.backgroundColor = "black";
-            setTimeout(() => {
-                element.style.backgroundColor = color;
-            }, 200);
-                arr3.push(color);
-                console.log("arr3=",arr3);
-                if(arr2[c]!==arr3[c])
+            setTimeout(
+                () => 
                 {
-                    setMessage("You lost the game please start again");
-                    alert("game over you lost it")
-                    window.location.reload();
-                    return
-                }
-                else{
-                setC(prev=>prev+1);
-                if(c===count)
-                    {
-                        setCount(prev=>prev+1);
-                        setVis(false);
-                        setMessage(`Level ${count}`);
-                        alert(`level ${count}`)
-                        setC(0);
-                        setTimeout(()=>game(),2000);
-                    }
+                    glowColors(totalCount, currentIndex + 1);
+                }, 1500);
+        }    
+        else 
+        {
+            console.log(arr2,"noted indside glow colors");
+            vis=true;
+        }
+    }
+
+    function glowAndReset(element, color)
+    {
+        element.current.style.backgroundColor = "black";
+        setTimeout(() => 
+        {
+            element.current.style.backgroundColor = color;
+        }, 1000);
+    }
+
+    function coloor(element,color)
+    {   
+        if(vis)
+          console.log(vis,"=vis")
+        {
+           ()=> element.current.style.backgroundColor = "black";
+        setTimeout(() => {
+            element.current.style.backgroundColor = color;
+        }, 200);
+            arr3.push(color);
+            console.log("arr3=",arr3);
+            if(arr2[c]!==arr3[c])
+            {
+                message="You lost the game please start again";
+                alert("game over you lost it")
+                window.location.reload();
+                return
+            }
+            else{
+            c=c+1;
+            if(c===count)
+                {
+                    count=count+1;
+                    vis=false;
+                    message=`Level ${count}`;
+                    alert(`level ${count}`)
+                    c=0;
+                    setTimeout(()=>game(),2000);
                 }
             }
         }
-    return(
-        <div className="main">
-        <div className="main-body">
-            <div className="red" onClick={()=>coloor(red,"red")}></div>
-            <div className="blue" onClick={()=>coloor(blue,"blue")}></div>
-        </div>
-        <div className="main-body">
-            <div className="green" onClick={()=>coloor(green,"green")}></div>
-            <div className="yellow" onClick={()=>coloor(yellow,"yellow")}></div>
-        </div>
-        <div className="main-body-buttons">
-          <button onClick={game}>Start</button>
-          <button onClick={()=>window.location.reload()}>Restart</button>
-          <p >{message}</p>
-        </div>
+    }
+return(
+    <div className="main">
+    <div className="main-body">
+        <div className="red" ref={red} onClick={()=>coloor(red,"red")}></div>
+        <div className="blue" ref={blue} onClick={()=>coloor(blue,"blue")}></div>
     </div>
-    )
+    <div className="main-body">
+        <div className="green" ref={green} onClick={()=>coloor(green,"green")}></div>
+        <div className="yellow" ref={yellow} onClick={()=>coloor(yellow,"yellow")}></div>
+    </div>
+    <div className="main-body-buttons">
+      <button onClick={game}>Start</button>
+      <button onClick={()=>window.location.reload()}>Restart</button>
+      <p>{message}</p>
+    </div>
+</div>
+)
 }
 export default Mindgame;
